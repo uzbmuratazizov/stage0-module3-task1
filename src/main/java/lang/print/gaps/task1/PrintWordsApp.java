5c21569e9ca01fe8386c25d6b845d32624559aac
uzbmuratazizov <82669801+uzbmuratazizov@users.noreply.github.com>
2022-08-16 12:58:57 +0500

    Update PrintWordsApp.java

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("Java \nis \nGreat!");
+        System.out.println("Java \nis \nsmth");
     }
 }
